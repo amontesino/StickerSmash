8338e6da4f24c24a13a9b7743c093c97e13c6e59
Armando Montesino <armandomontesino98@gmail.com>
2025-07-31 18:13:36 -0400

    finished chapters 1 and 2

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, View } from "react-native";
+
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  }
+})
