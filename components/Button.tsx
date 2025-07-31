3938e73196244a559fba199cba1dee63d7bddba2
Armando Montesino <armandomontesino98@gmail.com>
2025-07-31 19:34:03 -0400

    added button in ch 3

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,37 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 type Props = {
     label: string;
+    theme?: 'primary';
 }
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+    if (theme === 'primary') {
+        return (
+            <View
+                style={[
+                    styles.buttonContainer,
+                    { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18},
+                ]}>
+                <Pressable
+                    style={[styles.button, { backgroundColor: '#fff' }]}
+                    onPress={() => alert('You pressed a button.')}>
+                    <FontAwesome name="picture-o" size={18} color='#25292e' style={styles.buttonIcon} />
+                    <Text style={styles.buttonLabel}>{label}</Text>
+                </Pressable>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
-                <Text style={styles.buttonLabel}>{label}</Text>
-            </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Text style={styles.buttonLabel}>{label}</Text>
+        </Pressable>
         </View>
     )
+    
 }
 
 const styles = StyleSheet.create({
@@ -30,6 +50,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row'
+    },
+    buttonIcon: {
+        paddingRight: 8
     },
     buttonLabel: {
         color: '#fff',
